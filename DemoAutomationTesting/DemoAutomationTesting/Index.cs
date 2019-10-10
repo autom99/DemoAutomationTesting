e7e5d9f01a61680cf50982f72258daaea7600e04
Autom99 <ehardik92@gmail.com>
2019-10-10 15:23:25 +0530

    Add files via upload

---
+++ DemoAutomationTesting/DemoAutomationTesting/Index.cs
@@ -0,0 +1,39 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace DemoAutomationTesting
+{
+    class Index
+    {
+        IWebDriver driver;
+
+        [SetUp]
+        public void StartBrowser()
+        {
+            driver = new ChromeDriver(@"D:\\Testing Purpose");
+        }
+
+        [Test]
+        public void Test()
+        {
+            driver.Url = "http://demo.automationtesting.in/Index.html";
+
+            driver.FindElement(By.XPath("//input[@type='text'][@id='email']")).SendKeys("test@gmail.com");
+            driver.FindElement(By.XPath("//img[@id='enterimg']")).Click();
+            driver.Navigate().Back();
+
+            driver.FindElement(By.XPath("//button[text()='Sign In']")).Click();
+            driver.Navigate().Back();
+
+            driver.FindElement(By.XPath("//button[text()='Skip Sign In']")).Click();
+            driver.Navigate().Back();
+        }
+
+        [TearDown]
+        public void CloseBrowser()
+        {
+            driver.Close();
+        }
+    }
+}
